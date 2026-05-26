a74c7f5a4b9415ccd207dfb01d5d8338fc7c6bca
max <maximiliano.arriapia@gmail.com>
2026-05-26 14:02:56 -0400

    vuelta

--- src/AnalyticsPage.tsx
+++ src/AnalyticsPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Analytics from './Analytics';
+import Analytics2 from './Analytics2';
 
 const rootElement = document.getElementById('analytics-root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <Analytics />
+      <Analytics2 />
     </React.StrictMode>
   );
 }
