2ae989b8fbb8b1928ac9b0714f97da4e838ba181
max <maximiliano.arriapia@gmail.com>
2026-05-27 16:28:08 -0400

    volvemos a intentar

--- src/comentsPage.tsx
+++ src/comentsPage.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Coments from './coments';
 
 const rootElement = document.getElementById('coments-root');
+
 if (rootElement) {
     ReactDOM.createRoot(rootElement).render(
         <React.StrictMode>
