ef7e221396d9600d5fe596175c6dea0f1e6b9e39
max <maximiliano.arriapia@gmail.com>
2026-05-27 16:19:18 -0400

    new coments page

---
+++ src/comentsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Coments from './coments';
+
+const rootElement = document.getElementById('coments-root');
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <Coments />
+        </React.StrictMode>
+    );
+}
